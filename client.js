4ed501ca9f23bc5641ae3b96c920be3cd1b51b3d
Keriann Barault-Lindor <keriannbl@hotmail.com>
2017-03-28 15:52:27 -0400

    ajout d'une feuille de style externe, modification du fichier index_1.ejs

--- client.js
+++ client.js
@@ -1,1 +1,29 @@
 //AJAX
+xhr = new XMLHttpRequest();
+xhr.open('POST', "modifier", true);
+data = { 
+ "modif":{
+ "nom" : elmLigneDiv[0].innerHTML,
+ "prenom" : elmLigneDiv[1].innerHTML,
+ "telephone" : elmLigneDiv[2].innerHTML
+ },
+ "_id" : elmLigneDiv[3].innerHTML 
+ }
+}
+sData = JSON.stringify(data);
+xhr.setRequestHeader('Content-type', 'application/json');
+xhr.send(sData);
+xhr.addEventListener("readystatechange", traiterRequest, false);
+
+function traiterRequest(e){
+ console.log("xhr.readyState = " + xhr.readyState)
+ console.log("xhr.status = " + xhr.status)
+ if(xhr.readyState == 4 && xhr.status == 200){
+	 console.log('ajax fonctionne')
+	 var response = JSON.parse(xhr.responseText);
+	 console.log(xhr.responseText);
+	 elmChamp_id.innerHTML = response[0]._id
+	 elmLigne.style.backgroundColor = "#0f0"
+
+	}
+ }
